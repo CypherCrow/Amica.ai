399391784dd7d8385c8114024090e9d5d32dfd7a
Jonah Gonzalez <jonah3254@gmail.com>
2022-05-05 01:12:00 -0700

    Huge update to project: uikit installed, portal page updated, carousel in progress

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,7 +2,6 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms'; 
 import { RouterModule } from '@angular/router'; 
-import { MdbCarouselModule } from 'mdb-angular-ui-kit/carousel'; 
 
 import { AppComponent } from './app.component';
 import { MessagesComponent } from './messages/messages.component';
@@ -14,20 +13,6 @@ import { AboutComponent } from './pages/about/about.component';
 import { LoginComponent } from './pages/login/login.component';
 import { HomepageComponent } from './pages/homepage/homepage.component';
 import { HowToComponent } from './pages/how-to/how-to.component';
-import { MdbAccordionModule } from 'mdb-angular-ui-kit/accordion';
-import { MdbCheckboxModule } from 'mdb-angular-ui-kit/checkbox';
-import { MdbCollapseModule } from 'mdb-angular-ui-kit/collapse';
-import { MdbDropdownModule } from 'mdb-angular-ui-kit/dropdown';
-import { MdbFormsModule } from 'mdb-angular-ui-kit/forms';
-import { MdbModalModule } from 'mdb-angular-ui-kit/modal';
-import { MdbPopoverModule } from 'mdb-angular-ui-kit/popover';
-import { MdbRadioModule } from 'mdb-angular-ui-kit/radio';
-import { MdbRangeModule } from 'mdb-angular-ui-kit/range';
-import { MdbRippleModule } from 'mdb-angular-ui-kit/ripple';
-import { MdbScrollspyModule } from 'mdb-angular-ui-kit/scrollspy';
-import { MdbTabsModule } from 'mdb-angular-ui-kit/tabs';
-import { MdbTooltipModule } from 'mdb-angular-ui-kit/tooltip';
-import { MdbValidationModule } from 'mdb-angular-ui-kit/validation';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { PortalComponent } from './pages/portal/portal.component';
 
@@ -44,29 +29,14 @@ import { PortalComponent } from './pages/portal/portal.component';
   imports: [
     BrowserModule, 
     FormsModule, 
-    AppRoutingModule, 
-    MdbCarouselModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,  
     RouterModule.forRoot([
       { path: '', component: HomepageComponent }, 
       { path: 'how-to', component: HowToComponent }, 
       { path: 'about', component: AboutComponent },
       { path: 'portal', component: PortalComponent },
     ]),
-    MdbAccordionModule,
-    MdbCheckboxModule,
-    MdbCollapseModule,
-    MdbDropdownModule,
-    MdbFormsModule,
-    MdbModalModule,
-    MdbPopoverModule,
-    MdbRadioModule,
-    MdbRangeModule,
-    MdbRippleModule,
-    MdbScrollspyModule,
-    MdbTabsModule,
-    MdbTooltipModule,
-    MdbValidationModule,
-    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
