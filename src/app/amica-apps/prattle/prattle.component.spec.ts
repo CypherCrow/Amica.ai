53589b7fc5e78ac5f42b0b938a9a1d5cbebc1c24
Jonah "Jon" Morgan <57231486+CypherCrow@users.noreply.github.com>
2022-05-05 18:10:28 -0700

    MAJOR UPDATE

---
+++ src/app/amica-apps/prattle/prattle.component.spec.ts
@@ -0,0 +1,25 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { PrattleComponent } from './prattle.component';
+
+describe('PrattleComponent', () => {
+  let component: PrattleComponent;
+  let fixture: ComponentFixture<PrattleComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ PrattleComponent ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(PrattleComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ src/app/amica-apps/prattle/prattle.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-prattle',
+  templateUrl: './prattle.component.html',
+  styleUrls: ['./prattle.component.css']
+})
+export class PrattleComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
